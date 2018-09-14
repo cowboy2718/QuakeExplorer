0a79bdcdbf273e99a4daffd22b106fd98c560ffd
Gojanovic <GY28@millercoors.com>
2018-09-13 21:16:52 -0600

    add label plot test

--- tests/testthat/test_time_series_plotting.R
+++ tests/testthat/test_time_series_plotting.R
@@ -3,6 +3,7 @@ library(readr)
 library(dplyr)
 library(ggplot2)
 library(lubridate)
+
 context("checking time series plotting")
 
 results <- read_delim('results.csv', delim = '\t')
@@ -13,5 +14,16 @@ ggplot() + geom_timeline(aes(x = datevalue, y = COUNTRY, color = TOTAL_DEATHS, s
 
 test_that("time series only",{
   expect_is(test_plt, "ggplot")
+})
+
+test_plot2<- results %>% filter(COUNTRY =="CHINA" | COUNTRY=="USA" | COUNTRY=="GREECE") %>% 
+filter(datevalue > '1900-01-01', datevalue < '1950-01-01') %>% 
+ggplot() + geom_timeline(aes(x = datevalue, y = COUNTRY, color = TOTAL_DEATHS, size = EQ_PRIMARY)) +
+  geom_timeline_label(data=results,aes(x = datevalue, y = COUNTRY, magnitude = EQ_PRIMARY,label = location_name, max_labels= 5))
+ 
+
+test_that("labeled time series plot",{
+  expect_is(test_plot2, "ggplot")
+  
 })
 
